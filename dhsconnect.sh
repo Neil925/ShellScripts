53dfef198909fd9f91b24b292e4b8d63b13b7ba6
Neil E. <elkadineil@gmail.com>
2024-07-08 16:03:50 -0400

    Better dhsconnect

--- dhsconnect.sh
+++ dhsconnect.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-var=$(nordvpn status)
-
-if [[ $var =~ "Disconnected" ]]; then
-    ssh -i '/home/neil/Sensative/.ssh/archpckey' dhs@192.168.1.219
+if [ -z $1 ]; then
+    echo -n "(L)ocal?/(r)emote: "
+    read loc
+    if [ -z loc ] || [[ $loc == "" ]]; then
+        loc=r
+    fi
 else
-    ssh -i '/home/neil/Sensative/.ssh/archpckey' dhs@72.239.188.77
+    loc=$1
 fi
+
+if [[ $loc == "r" ]] || [[ $loc == "R" ]]; then
+    ssh -i '/home/neil/Sensative/.ssh/archlpkey' dhs@72.239.163.236
+elif [[ $loc == "l" ]] || [[ $loc == "L" ]]; then
+    ssh -i '/home/neil/Sensative/.ssh/archlpkey' dhs@192.168.1.2
+else
+    echo "Invalid.";
+    exit 1;
+fi
+
+echo "Connection closed. Press anything to continue."
+read -n 1
 
 exit 0
